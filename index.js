b82d63c5a8c300b9614cb0f30ca6f335e197c090
spacezus <76877935+spacezur@users.noreply.github.com>
2022-12-30 21:31:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const {Client, Events, GatewayIntentBits} = require('discord.js')
+const client = new Client({
+    intents : [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages
+    ]
+})
+const {token} = require('./config.json')
+
+client.once(Events.ClientReady, () => {
+	console.log(`Ready! Logged in as ${client.user.tag}`)
+})
+
+client.on(Events.MessageCreate, async interaction => {
+    if (interaction.webhookId == null) {
+        if (!interaction.author.bot) {
+            interaction.delete()
+        }
+        return
+    }
+
+    if (interaction.channelId == '1058420655579738162') {
+        interaction.channel.send('<@628959628780240906>').then(msg => msg.delete())
+        setTimeout(function() {
+            interaction.delete()
+        }, 12 * (1000 * 60 * 60))
+    }
+
+    if (interaction.channelId == '1058420590689669131') {
+        setTimeout(function() {
+            interaction.delete()
+        }, 5 * (1000 * 60))
+    }
+})
+
+client.login(token)
